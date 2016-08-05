e44ed5a022bdf3d9ad6cc78d6cce227db51f31fa
Alexander Lloyd <alexander.lloyd@crisalix.com>
2016-08-05 18:53:29 +0200

    first commit

---
+++ src/js/entry.js
@@ -0,0 +1,9 @@
+// Import CSS
+import '../css/master.scss';
+
+// Import React and JS
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
